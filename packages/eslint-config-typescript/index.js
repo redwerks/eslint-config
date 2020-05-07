786f509119c9ebc19d9e9c6f486df77f00cb21ee
Daniel Friesen <d@danf.ca>
2020-05-06 22:32:02 -0700

    Loosen up the default @typescript-eslint/explicit-function-return-type rule

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -7,5 +7,16 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json'
   },
-  plugins: ['@typescript-eslint']
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      {
+        // There's no good reason to be strict about return types where it's already explicitly defined elsewhere
+        allowExpressions: true,
+        allowTypedFunctionExpressions: true,
+        allowHigherOrderFunctions: true
+      }
+    ]
+  }
 };
