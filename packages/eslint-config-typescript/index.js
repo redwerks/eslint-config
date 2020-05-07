5611a5eb260c01ef4d094b580c067b6ee2f63485
Daniel Friesen <d@danf.ca>
2020-05-06 23:04:20 -0700

    Move eslint-config-typescript config to overrides
    This will allow for the typescript config to be used without messing up .js files due to TypeScript-only rules incorrectly applying to .js file.
    
    Redwerks and clients have plenty of mixed TS/JS codebases. And build/config scripts (e.g. eslint and babel config) are typically .js not .ts even in full-ts codebases. So it doesn't make sense to apply TypeScript rules globally now that eslint supports extending overrides and the plugin's developers are not willing to make TypeScript rules not apply to JS files.

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,25 +1,30 @@
 module.exports = {
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-  ],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    '@typescript-eslint/explicit-function-return-type': [
-      'warn',
-      {
-        // There's no good reason to be strict about return types where it's already explicitly defined elsewhere
-        allowExpressions: true,
-        allowTypedFunctionExpressions: true,
-        allowHigherOrderFunctions: true,
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+      ],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': [
+          'warn',
+          {
+            // There's no good reason to be strict about return types where it's already explicitly defined elsewhere
+            allowExpressions: true,
+            allowTypedFunctionExpressions: true,
+            allowHigherOrderFunctions: true,
+          },
+        ],
+        // There are perfectly good reasons to write empty functions
+        '@typescript-eslint/no-empty-function': 'off',
       },
-    ],
-    // There are perfectly good reasons to write empty functions
-    '@typescript-eslint/no-empty-function': 'off',
-  },
+    },
+  ],
 };
